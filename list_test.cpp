a7646b4ecd8ec43bac620249aecb34859c6b7019
David Axelsson <davidaxelsson5@gmail.com>
2022-09-20 16:14:11 +0200

    Första testen

---
+++ list_test.cpp
@@ -0,0 +1,111 @@
+#include "catch.hpp"
+#include "list.h"
+#include <utility>
+
+TEST_CASE("Constructors and getters")
+{
+    SECTION("Default constructor")
+    {
+        List list{};
+
+        CHECK_THROWS(list.get(0));
+        CHECK_THROWS(list.front());
+        CHECK_THROWS(list.back());
+    }
+    SECTION("Integer constructor")
+    {
+        List list{1, 2, 3};
+
+        CHECK_THROWS(list.get(-1));
+        CHECK_THROWS(list.get(3));
+
+        CHECK(list.get(0) == 1);
+        CHECK(list.get(1) == 2);
+        CHECK(list.get(2) == 3);
+
+        CHECK(list.front() == 1);
+        CHECK(list.back() == 3);
+    }
+    SECTION("Copy constructor")
+    {
+        List l1 {1, 2, 3};
+        List l2 {l1};
+        l2.push_back(4);
+        
+        CHECK(l1.to_string() == "[1, 2, 3]");
+        CHECK(l2.to_string() == "[1, 2, 3, 4]");
+    }
+    SECTION("Assignment operator")
+    {
+        List l1{1, 2, 3};
+        List l2{};
+        l2 = l1;
+        CHECK(l1.to_string() == "[1, 2, 3]");
+        CHECK(l2.to_string() == "[1, 2, 3, 4]");
+    }
+    SECTION("MOVE")
+    {
+        List l1{std::move(1, 2, 3)};
+        CHECK(l1.to_string() == "[1, 2, 3]");
+    }
+}
+TEST_CASE("to_string method")
+{
+    SECTION("To string")
+    {
+        List empty{};
+        List non_empty{1, 2, 3};
+        CHECK(empty == "[]");
+        CHECK(non_empty == "[1, 2, 3]");
+    }
+}
+TEST_CASE("Add/remove values")
+{
+    SECTION("Add, push_back, push_front")
+    {
+        List l1{};
+        l1.push_back(1); 
+        CHECK(l1.to_string() == "[1]"); 
+        l1.push_front(2); 
+        CHECK(l1.to_string == "[2, 1]"); 
+        l1.push_back(3); 
+        CHECK(l1.to_string == "[2, 1, 3]"); 
+    }
+    SECTION("Remove, pop_back, pop_front")
+    {
+        List l1{1, 2, 3};
+        List l2{1, 2, 3};
+        List l3{};
+
+        CHECK_THROWS(l3.pop_back());
+        CHECK_THROWS(l3.pop_front());
+
+        CHECK(l1.pop_back() == 3);
+        CHECK(l1.to_string() == "[1, 2]");
+
+        CHECK(l2.pop_front() == 1);
+        CHECK(l1.to_string() == "[2, 3]");
+    }
+    SECTION("Empty, length")
+    {
+        List l1{};
+        List l2{1};
+
+        CHECK_FALSE(l2.empty());
+        CHECK(l1.empty());
+        l2.pop_back();
+        l1.push_back(1);
+        CHECK_FALSE(l1.empty());
+        CHECK(l2.empty());
+
+        CHECK(l2.length() == 0)
+        CHECK(l1.length() == 1)
+        
+    }
+    SECTION("Clear list, clear")
+    {
+        List l1 {1, 2, 3, 4};
+        l1.clear();
+        CHECK(l1.empty());
+    }
+}
